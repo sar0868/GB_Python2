33d80573f81034735bc7f842b065029551f7b16a
sar <safarov-ar@yandex.ru>
2017-03-30 14:59:11 +0300

    Приведение файла в порядок по pep8

--- lesson_1/homework/pep8_me.py
+++ lesson_1/homework/pep8_me.py
@@ -3,24 +3,28 @@ import string
 
 
 def create_file(namef,dir,size):
-        if(size.isdigit()!=True):
-                if size.endswith('KB')==True:
-                        s1 = size.split('KB')
-                        size1 = int(s1[0])*1024
-                        token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(size1))
-                if size.endswith('MB')==True:
-                        s1 = size.split('MB')
-                        size1 = int(s1[0])*1048567
-                        token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(size1))
-                if size.endswith('GB') == True:
-                        s1 = size.split('GB')
-                        size1 = int(s1[0]) * 1073741824
-                        token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(size1))
-        else:
-                token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(int(size)))
+    if(size.isdigit()!=True):
+        if size.endswith('KB')==True:
+            s1 = size.split('KB')
+            size1 = int(s1[0])*1024
+            token = ''.join(random.choice(string.ascii_uppercase +
+                                          string.ascii_lowercase + string.digits) for x in range(size1))
+        if size.endswith('MB')==True:
+            s1 = size.split('MB')
+            size1 = int(s1[0])*1048567
+            token = ''.join(random.choice(string.ascii_uppercase + \
+                                    string.ascii_lowercase + string.digits) for x in range(size1))
+        if size.endswith('GB') == True:
+            s1 = size.split('GB')
+            size1 = int(s1[0]) * 1073741824
+            token = ''.join(random.choice(string.ascii_uppercase + \
+                string.ascii_lowercase + string.digits) for x in range(size1))
+    else:
+        token = ''.join(random.choice(string.ascii_uppercase + \
+            string.ascii_lowercase + string.digits) for x in range(int(size)))
 
-        file = open(dir+namef,"w")
-        file.write(token)
+file = open(dir + namef, "w")
+file.write(token)
 
 
 create_file("/test1.txt","E:",'10KB')
