9715c9a61e40f7970539f9b6c396e82611c85b8f
Aleksey Safarov <safarov-ar@yandex.ru>
2017-04-04 00:30:00 +0300

    lesson1, распаковал и что сравнивать с md5 незнаю

--- lesson_1/homework/collect.py
+++ lesson_1/homework/collect.py
@@ -5,13 +5,30 @@ import zipfile
 
 def unpack():
     files = os.listdir('files')
-    print(files)
     for file in files:
         if file[-3:] == 'zip':
             dir_name = os.path.join('files/',file[:-4])
-            os.makedirs(dir_name)
-            unpack_files = zipfile.ZipFile(file, "r")
-            unpack_files.extractall()
+            # os.makedirs(dir_name)
+            path_zip = os.path.join('files/',file)
+            zipOb = zipfile.ZipFile(path_zip)
+            zipOb.extractall('files')
+            zipOb.close()
+            curent_files = os.listdir(dir_name)
+            os.chdir(dir_name)
+            for cur_file in curent_files:
+                if cur_file[-3:] != 'md5':
+                    # with open(cur_file, 'r') as f:
+                    #     text = f.read()
+                    #     text = bytes(text, 'utf-8')
+                    #     h = hashlib.md5(text)
+                    #     res = h.hexdigest()
+                    # with open(cur_file, 'w') as f:
+                    #     f.write(res)
+            os.chdir('..')
+            os.chdir('..')
+
+
+
 
 
 unpack()
