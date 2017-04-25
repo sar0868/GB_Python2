e7311ab04b20e7ab3a7b668bb4f96daa8d3aed32
Aleksey Safarov <safarov-ar@yandex.ru>
2017-04-25 23:41:57 +0300

    создал класс, надо дописать

--- lesson_2/homework/client.py
+++ lesson_2/homework/client.py
@@ -6,6 +6,21 @@ import string
 import random
 import datetime
 
+class Transaction():
+
+    def __init__(self):
+        pass
+
+    def pack(self):
+        # title = '{}'.format(bin('zz'))
+        data = datetime.datetime.today()
+        yearT = '{:07b}'.format(data.year - 2000)
+        dayT = '{:05b}'.format(data.day)
+        monthT = '{:04b}'.format(data.month)
+        data_trans = ''.join(yearT,monthT,dayT)
+        return data_trans
+
+
 
 def randstring(n):
     a = string.ascii_letters + string.digits
@@ -17,8 +32,6 @@ def packet_transaction():
     title = '0x7A7A'
     data_trans = datetime.datetime.today()
     res = bytes('{}{}{}'.format(title, data_trans, data),'utf-8')
-
-
     return res
 
 
@@ -37,6 +50,3 @@ print(recvd)
 sock.close()
 
 # __add__  ->  +
-
-
-
